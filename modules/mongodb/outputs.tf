cdb1559814302e8da22b0097329584140f175734
Snuffules <53743351+Snuffules@users.noreply.github.com>
2023-12-03 21:00:46 +0200

    Update outputs.tf

--- modules/mongodb/outputs.tf
+++ modules/mongodb/outputs.tf
@@ -4,6 +4,5 @@ output "load_balancer_ip" {
 
 output "k8s_endpoint_ip" {
    value = kubernetes_service.mongo_lb_svc.spec[0].cluster_ip
-#  value = [for s in kubernetes_endpoints.mongodb_ep.subset : s.address if length(s.address) > 0][0]
-  description = "IP address of the Kubernetes Endpoint"
+   description = "IP address of the Kubernetes Endpoint"
 }
