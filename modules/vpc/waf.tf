ca032b30cdeca56399903e5c66c8d8fa42fa7ec0
Snuffules <53743351+Snuffules@users.noreply.github.com>
2023-12-03 21:37:23 +0200

    Add files via upload

---
+++ modules/vpc/waf.tf
@@ -0,0 +1,28 @@
+resource "google_compute_security_policy" "waf_policy" {
+  name        = "waf-policy"
+  description = "Web Application Firewall Policy"
+
+  rule {
+    action   = "allow"
+    priority = "1000"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["10.8.0.0/28", "10.0.0.0/24", "10.0.1.0/28", var.load_balancer_ip]
+      }
+    }
+    description = "Allow traffic from specific IP range"
+  }
+
+  rule {
+    action   = "deny(403)"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "Deny all other traffic"
+  }
+}
