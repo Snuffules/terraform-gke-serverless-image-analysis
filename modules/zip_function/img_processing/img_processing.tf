6aa698bfd64517ea9985547be7de55e66ed36526
Snuffules <53743351+Snuffules@users.noreply.github.com>
2023-12-03 22:02:35 +0200

    Update img_processing.tf

--- modules/zip_function/img_processing/img_processing.tf
+++ modules/zip_function/img_processing/img_processing.tf
@@ -73,67 +73,9 @@ resource "google_storage_bucket" "safe_storage_function" {
   }  
 }
 
-data "google_storage_project_service_account" "default" {
-}
-
-resource "google_project_iam_member" "gcs_pubsub_publishing" {
-  project = var.project_id
-  role    = "roles/pubsub.publisher"
-  member  = "serviceAccount:${data.google_storage_project_service_account.default.email_address}"
-}
-
-# Permissions on the service account used by the function and Eventarc trigger
-resource "google_project_iam_member" "invoking" {
-  project    = var.project_id
-  role       = "roles/run.invoker"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.gcs_pubsub_publishing]
-}
-
-resource "google_project_iam_member" "event_receiving" {
-  project    = var.project_id
-  role       = "roles/eventarc.eventReceiver"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.invoking]
-}
-
-resource "google_project_iam_member" "artifactregistry_reader" {
-  project    = var.project_id
-  role       = "roles/artifactregistry.reader"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.event_receiving]
-}
-
-resource "google_project_iam_member" "connectors_admin" {
-  project    = var.project_id
-  role       = "roles/connectors.admin"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.event_receiving]
-}
-
-resource "google_project_iam_member" "secretmanager_viewer" {
-  project    = var.project_id
-  role       = "roles/secretmanager.viewer"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.event_receiving]
-}
-
-resource "google_project_iam_member" "secretmanager_secretAccessor" {
-  project    = var.project_id
-  role       = "roles/secretmanager.secretAccessor"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.event_receiving]
-}
-
-resource "google_project_iam_member" "storage_admin" {
-  project    = var.project_id
-  role       = "roles/storage.admin"
-  member     = "serviceAccount:${var.service_account}"
-  depends_on = [google_project_iam_member.event_receiving]
-}
-
 ###########################################################
-
+# Archiving and storing function on cloud storagea as .zip
+###########################################################
 data "archive_file" "img_processing" {
   type        = "zip"
   output_path = "/tmp/function-source-img_processing.zip"
@@ -178,9 +120,10 @@ resource "google_cloudfunctions2_function" "img_processing" {
     ingress_settings               = var.ingress_settings
     service_account_email          = var.service_account
   }
-#####################################################
-#  official guidance from google for strorage trigger
-######################################################
+  
+###################
+# Strorage trigger
+###################
 
   event_trigger {
     trigger_region        = "europe-west1"
