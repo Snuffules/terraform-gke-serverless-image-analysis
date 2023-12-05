f7edaa8c71bb198a21832339e8502e2ab2fef0f5
Snuffules <53743351+Snuffules@users.noreply.github.com>
2023-12-05 13:03:06 +0200

    Update waf.tf

--- modules/vpc/waf.tf
+++ modules/vpc/waf.tf
@@ -8,7 +8,7 @@ resource "google_compute_security_policy" "waf_policy" {
     match {
       versioned_expr = "SRC_IPS_V1"
       config {
-        src_ip_ranges = ["10.8.0.0/28", "10.0.0.0/24", "10.0.1.0/28", var.load_balancer_ip]
+        src_ip_ranges = ["10.8.0.0/28", "10.0.0.0/24", "10.0.1.0/28", var.load_balancer_ip] # # vpc, subnet, load balancer and gke master ipv4 cidr range 
       }
     }
     description = "Allow traffic from specific IP range"
