9c5020dc93cfff4e922f0a243ea1ad89e36fec66
Snuffules <53743351+Snuffules@users.noreply.github.com>
2023-12-05 13:02:42 +0200

    Update firewall.tf

--- modules/vpc/firewall.tf
+++ modules/vpc/firewall.tf
@@ -8,5 +8,5 @@ resource "google_compute_firewall" "mongodb_firewall" {
     protocol = "tcp"
     ports    = ["27017"]
   }
-  source_ranges = ["10.8.0.0/28", "10.0.0.0/24", "10.0.1.0/28", var.load_balancer_ip] #"10.68.0.42"] #, var.k8s_ep ] # vpc, k8s endpoint and gke master ipv4 cidr range 
+  source_ranges = ["10.8.0.0/28", "10.0.0.0/24", "10.0.1.0/28", var.load_balancer_ip] #"10.68.0.42"] # vpc, subnet, load balancer and gke master ipv4 cidr range 
 }
